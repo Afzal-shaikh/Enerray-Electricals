4359ac908cc45a659834291a8670e6409e8d74a3
netrat <shaikhafzal9900@gmail.com>
2021-01-03 23:18:50 +0530

    audio player slider functionality done'

--- EnerrayAndroid/components/AudioPlayer.js
+++ EnerrayAndroid/components/AudioPlayer.js
@@ -8,13 +8,18 @@ import {
   StyleSheet,
 } from 'react-native';
 import React ,{useEffect, useState} from 'react';
-
+import SoundPlayer from 'react-native-sound-player'
 
 const img_speaker = require('../assets/ui_speaker.png');
 const img_pause = require('../assets/ui_pause.png');
 const img_play = require('../assets/ui_play.png');
 const img_playjumpleft = require('../assets/ui_playjumpleft.png');
 const img_playjumpright = require('../assets/ui_playjumpright.png');
+
+const STOPPED = "stopped";
+const PAUSED = "paused";
+const PLAYING = "playing";
+
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -24,25 +29,78 @@ export default function AudioPlayer(props) {
 
 let audio 
 useEffect(()=>{
-   
+  SoundPlayer.loadUrl(audioUrl)
+ let  _onFinishedLoadingURLSubscription = SoundPlayer.addEventListener('FinishedLoadingURL', ({ success, url }) => {
+    console.log('finished loading url', success, url)
+  })
 
+  const interval = setInterval(() => sliderEditing ? console.log("slider editing") : getPlayerInfo() , 1000);
+
+  return () => {
+    SoundPlayer.stop()
+    _onFinishedLoadingURLSubscription.remove()
+    clearInterval(interval);
+  }
 },[]);
 
-
-const [isPlaying, setisPlaying] = useState(false)
-// const isPlaying = false
+//state
+const [playingState, setplayingState] = useState(STOPPED)
+const [currentTime, setCurrentTime] = useState(0)
+const [duration, setDuration] = useState(0)
+const [sliderEditing, setsliderEditing] = useState(false)
+// const [time, setTime] = useState(Date.now());
 
 
   const audioUrl =
     'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3';
 
-
-
-    
     // events
-    function onPlayPauseClicked(){
-     
+
+     async function getPlayerInfo  (){
+     let playerInfo = await SoundPlayer.getInfo()
+     console.log(playerInfo)
+     setCurrentTime( playerInfo.currentTime)
+     setDuration(playerInfo.duration)
     }
+
+    function onPlayPauseClicked(){
+
+      switch(playingState){
+        case STOPPED : 
+        SoundPlayer.play();
+        setplayingState(PLAYING);
+        break;
+
+        case PAUSED :
+        SoundPlayer.play()
+        setplayingState(PLAYING);
+        break;
+
+        case PLAYING :
+          SoundPlayer.pause();
+          setplayingState(PAUSED);
+         
+        break;
+        
+        
+
+        default:
+          console.log("Audio player default");
+          break;
+      }
+    }
+
+
+    //functions
+    function secsToMin(time) {
+      var minutes = Math.floor(time / 60);
+      var seconds =   Math.floor (time - (minutes * 60))
+      return {
+        minutes : minutes,
+        seconds : seconds
+      }
+    }
+
 
   return (
     <View
@@ -54,17 +112,28 @@ const [isPlaying, setisPlaying] = useState(false)
         <Slider
           style={styles.slider}
           minimumValue={0}
-          maximumValue={1}
+          maximumValue={duration}
+          value={currentTime}
+          onValueChange = { val => setCurrentTime(val) }
+          onSlidingComplete = {() => {SoundPlayer.seek(currentTime)
+          SoundPlayer.play()
+          }}
           minimumTrackTintColor="#FFFFFF"
           maximumTrackTintColor="#000000"
+          onTouchStart = {() => setsliderEditing(true)}
+          onTouchEnd = {() => {
+            setsliderEditing(false)
+           
+            console.log("curren time = " + currentTime)
+            }}
         />
       </View>
       <View style={styles.playerButtonView}>
-        <Text style={styles.timeText}>00:00</Text>
+        <Text style={styles.timeText}>{secsToMin(currentTime).minutes} : {secsToMin(currentTime).seconds}  </Text>
         <TouchableOpacity style={styles.button} onPress = {() => onPlayPauseClicked()  }>
-         <Image source={isPlaying ? img_pause : img_play} resizeMode={'contain'} style = {styles.playImage}/>
+         <Image source={(playingState == PLAYING ) ? img_pause : img_play} resizeMode={'contain'} style = {styles.playImage}/>
         </TouchableOpacity>
-        <Text style={styles.timeText}>00:00</Text>
+        <Text style={styles.timeText}>{secsToMin(duration).minutes} : {secsToMin(duration).seconds} </Text>
       </View>
     </View>
   );
@@ -100,8 +169,8 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.35,
   },
   timeText: {
-    backgroundColor: 'red',
-    fontSize : 15
+    // backgroundColor: 'red',
+    fontSize : 16
     
   },
   playerButtonView: {
