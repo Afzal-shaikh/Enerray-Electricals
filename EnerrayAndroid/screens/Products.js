5f961d666808e999039654345f9725d1098080a9
netrat <shaikhafzal9900@gmail.com>
2021-01-20 12:05:34 +0530

    products nmes added with new api integration

--- EnerrayAndroid/screens/Products.js
+++ EnerrayAndroid/screens/Products.js
@@ -1,118 +1,156 @@
-import React, { useEffect, useState  } from "react";
-import { StyleSheet, View, Text, FlatList, Dimensions, Image, ImageBackground ,ActivityIndicator, PlatformColor, Settings } from "react-native";
-import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+  Image,
+  ImageBackground,
+  ActivityIndicator,
+  PlatformColor,
+  Settings,
+} from 'react-native';
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 
-
-const { width, height } = Dimensions.get("window");
+const {width, height} = Dimensions.get('window');
 
 export default function Products(props) {
-
-  const productType = props.navigation.getParam("productType")
-  const allProductURL = `http://www.rudvedatrading.com/api/productshowall/${productType}`
+  const productType = props.navigation.getParam('productType');
+  const allProductURL = `http://www.rudvedatrading.com/api/productshowall/${productType}`;
   const [isLoading, setLoading] = useState(true);
-  const[data,setData] = useState([]);
-  console.log(data)
+  const [data, setData] = useState([]);
+  console.log(data);
 
-  useEffect(()=>{
-    callDataApi(allProductURL)
-  },[]);
+  useEffect(() => {
+    callDataApi(allProductURL);
+  }, []);
 
   return (
     <View style={styles.container}>
-
       {/* Search Bar */}
-      <View style={{ minHeight : 50 , shadowColor : "#ccc" , shadowOffset :{ width: 0, height: 2 }}}>
-          <TextInput 
-          style={{flex :1 ,padding : 5 , height : 40 , minHeight : 60 , fontSize : 20}} 
-          placeholder = "Search.."
-          onChangeText = {(text) => {
-            setLoading(true)
-            let searchUrl = `http://www.rudvedatrading.com/api/productsearch/${text}/${productType}`;
-            callDataApi(searchUrl)
-          }}
-          >
+      <View
+        style={{
+          minHeight: 50,
+          shadowColor: '#ccc',
+          shadowOffset: {width: 0, height: 2},
+        }}>
+        <TextInput
+          style={{flex: 1, padding: 5, height: 40, minHeight: 60, fontSize: 20}}
+          placeholder="Search.."
+          onChangeText={(text) => {
+            setLoading(true);
+            // let searchUrl = `http://www.rudvedatrading.com/api/productsearch/${text}/${productType}`;
+            let searchUrl = `http://www.rudvedatrading.com/api/productsearchpost`;
+            const requestOptions = {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                postsearch: `${text}`,
+                posttype: `${productType}`,
+              }),
+            };
 
-          </TextInput></View>
-
-  {isLoading ?
-  ( <View style = {{flex : 1, alignContent : "center",justifyContent : "center"}}>
-          <ActivityIndicator
-            animating= {true}
-            size="large"
-            color="#00ff00"
-          />
-        </View> ) : (
-    <View style={styles.childView}>
-    
-        <FlatList
-          // data={data.allproductdata.data} 
-         data = {data.hasOwnProperty('allproductdata') ? (data.allproductdata.data) : (data.searchproduct.data)}
-          renderItem={({ item }) => (
-            <TouchableOpacity style={styles.item} onPress = {() => { props.navigation.push("Description",{itemId : item.id})}}>
-            <ImageBackground style={styles.itemImage} source={{ uri: item.product_thumbnail_path }}>
-            <View style={{position: 'absolute', top: 5, left: 0,  justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{color : "grey"}} >{item.user_product_id}</Text>
-            </View>
-            <View style={{position: 'absolute', bottom: 5, left: 0,  justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{color : "grey"}} >ADD ITEM NAME</Text>
-            </View>
-            </ImageBackground>
-            </TouchableOpacity>
-          )}
-          numColumns={2}
-        />
+            fetch(searchUrl , requestOptions)
+              .then((response) => response.json())
+              .then((json) => setData(json))
+              .catch((error) => {
+                console.error(error);
+                callDataApi(allProductURL);
+              })
+              .finally(() => setLoading(false));
+          }}></TextInput>
       </View>
-  )}     
-    </View>
-  ); 
 
+      {isLoading ? (
+        <View
+          style={{flex: 1, alignContent: 'center', justifyContent: 'center'}}>
+          <ActivityIndicator animating={true} size="large" color="#00ff00" />
+        </View>
+      ) : (
+        <View style={styles.childView}>
+          <FlatList
+            // data={data.allproductdata.data}
+            data={
+              data.hasOwnProperty('allproductdata')
+                ? data.allproductdata.data
+                : data.searchproduct.data
+            }
+            renderItem={({item}) => (
+              <TouchableOpacity
+                style={styles.item}
+                onPress={() => {
+                  props.navigation.push('Description', {itemId: item.id});
+                }}>
+                <ImageBackground
+                  style={styles.itemImage}
+                  source={{uri: item.product_thumbnail_path}}>
+                  <View
+                    style={{
+                      position: 'absolute',
+                      top: 5,
+                      left: 0,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text style={{color: 'grey'}}>{item.user_product_id}</Text>
+                  </View>
+                  <View
+                    style={{
+                      position: 'absolute',
+                      bottom: 5,
+                      left: 0,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text style={{color: 'grey'}}>{item.product_name}</Text>
+                  </View>
+                </ImageBackground>
+              </TouchableOpacity>
+            )}
+            numColumns={2}
+          />
+        </View>
+      )}
+    </View>
+  );
 
   //api calling
 
- function callDataApi(url){
-  fetch(url)
-  .then((response) => response.json())
-  .then((json) =>  setData(json))
-  .catch((error) => {
-    console.error(error)
-    callDataApi(allProductURL)
-  })
-  .finally(() => setLoading(false));
- }
-
-
-
+  function callDataApi(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setLoading(false));
+  }
 }
 
 const itemMargin = 1;
-const itemWidth = width / 2 ;
+const itemWidth = width / 2;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     // paddingHorizontal: 20,
-    alignContent: "center",
-    justifyContent: "center",
-    display: "flex",
-   
-    
-
+    alignContent: 'center',
+    justifyContent: 'center',
+    display: 'flex',
   },
   item: {
     marginHorizontal: itemMargin,
     marginTop: itemMargin,
     width: itemWidth,
     height: itemWidth,
-    backgroundColor: "#ECECEC",
+    backgroundColor: '#ECECEC',
   },
   itemImage: {
-    backgroundColor: "#F1EAE9",
-    flex: 1
+    backgroundColor: '#F1EAE9',
+    flex: 1,
   },
   childView: {
     flex: 1,
-    
   },
 });
-
-
